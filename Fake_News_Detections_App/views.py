d652c2510a2b1cc63d70008747437e8deee9566a
kndbvortex <kamganguifo@gmail.com>
2022-04-19 05:24:27 +0100

    création de la page d'inscriptio et de connexion

--- Fake_News_Detections_App/views.py
+++ Fake_News_Detections_App/views.py
@@ -4,3 +4,10 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+
+def login(request):
+    return render(request, 'login.html')
+
+def register(request):
+    return render(request, 'register.html')
